3968b87f72fac2a5f90696e25e46db124e2946ba
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2023-11-07 20:36:20 +0000

    Comments to code

--- Source/AbstractionNewNEw/Private/MoveComponent.cpp
+++ Source/AbstractionNewNEw/Private/MoveComponent.cpp
@@ -37,23 +37,31 @@ void UMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
         FVector Direction = MoveDirection;
         float DeltaMove = Speed * DeltaTime;
 
+        //Determines the new location for this frame
         FVector NewLocation = CurrentLocation + Direction * DeltaMove;
+
+
         bool bReverse = false;
 
+        //Checks if the movement distance has been exceeded on the X
         if (FMath::Abs((NewLocation - OriginalLocation).X) > MoveDistanceX)
         {
+            //Sets NewLocation to maximum allowed distance of the X
             NewLocation.X = OriginalLocation.X + MoveDistanceX * FMath::Sign(Direction.X);
             bReverse = true;
         }
-
+        
+        //Checks if the movement distance has been exceeded on the Y
         if (FMath::Abs((NewLocation - OriginalLocation).Z) > MoveDistanceZ)
         {
+            //Sets NewLocation to maximum allowed distance of the Y
             NewLocation.Z = OriginalLocation.Z + MoveDistanceZ * FMath::Sign(Direction.Z);
             bReverse = true;
         }
 
         Owner->SetActorLocation(NewLocation);
 
+        //Reverses the direction by mutiplying the vector by -1
         if (bReverse)
         {
             MoveDirection *= -1;
